f0de60a2a3e53dc6be98c807ac6c068d210f9d01
tyler <tylertooxclusive@gmail.com>
2021-11-04 10:14:46 +0000

    data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { ChartData } from './Components/ChartData'
 
 export const App = () => {
   return (
     <div>
-      
+      {ChartData.map(chart => (
+        <div>
+          {chart.id}
+          {chart.songName}
+        </div>
+      ))}
     </div>
   )
 }
---
+++ src/Components/ChartData.jsx
@@ -0,0 +1,164 @@
+import AleYi from '../Assets/Img/AleYi.jpeg'
+ 
+export const ChartData = [
+    {
+      id:1,
+      change:1,
+      songName:'High',
+      artist:'Adekunle Gold, Davido',
+      onChart:'1',
+      imgUrl:AleYi 
+    },
+    {
+      id:2,
+      change:1,
+      songName:'Alcohol',
+      artist:'Joeboy',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:3,
+      change:1,
+      songName:'Monalisa',
+      artist:'Sarz, Lojay',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:4,
+      change:1,
+      songName:'Cold Outside',
+      artist:'Timaya, BUJU',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:5,
+      change:1,
+      songName:'Peru',
+      artist:'Fireboy DML',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:6,
+      change:1,
+      songName:'Egedege',
+      artist:'Larry Gaaga, Theresa Onuorah, Flavour,Phyno',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:7,
+      change:1,
+      songName:'Ozumba Mbadiwe',
+      artist:'Reekado Banks',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:8,
+      change:1,
+      songName:'Holy Father',
+      artist:'Mayorkun, Victony',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:9,
+      change:1,
+      songName:'Move',
+      artist:'Bad Boy Timz',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:10,
+      change:1,
+      songName:'Never Stopped',
+      artist:'Buju',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:11,
+      change:1,
+      songName:'Ale Yi',
+      artist:'Zlatan',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:12,
+      change:1,
+      songName:'Running',
+      artist:'LADIPOE, Fireboy DML',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:13,
+      change:1,
+      songName:'Bloody Samaritan',
+      artist:'Ayra Starr',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:14,
+      change:1,
+      songName:'Gone Far',
+      artist:'Zinoleesky',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:15,
+      change:1,
+      songName:'Levels',
+      artist:'Flavour',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:16,
+      change:1,
+      songName:'Lie',
+      artist:'Kizz Daniel',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:17,
+      change:1,
+      songName:'Somebody’s Son',
+      artist:'Tiwa Savage,Brandy',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:18,
+      change:1,
+      songName:'Understand',
+      artist:'Omah Lay',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:19,
+      change:1,
+      songName:'Mood',
+      artist:'WizKid, Buju',
+      onChart:'1',
+      imgUrl:'' 
+    },
+    {
+      id:20,
+      change:1,
+      songName:'Kilometer',
+      artist:'Buju',
+      onChart:'1',
+      imgUrl:'' 
+    },
+]
