668788845e80193e7df1ad357eebcdfa0b7612d4
tyler <tylertooxclusive@gmail.com>
2021-11-04 20:16:36 +0000

    more styling

--- src/Components/WeekOne.jsx
+++ src/Components/WeekOne.jsx
@@ -2,30 +2,42 @@ import { ChartData } from './ChartData'
 import './Chart.css'
 
 export const WeekOne = () => {
+
     return (
-        <div >
-            <div className='chart_container'>
-                <div className='chart_left' >
-                    <h3 className='chart_number'>Num</h3>
-                    <h3 className='chart_week'>Week</h3>
-                </div>
-                <div className='chart_song'>
-                    <div className='chart_song--info'>
-                       <h3>Song</h3>
+        <ul>
+            <li className='chart_container--text'>
+                <div className='chart_left'>
+                    <div className='chart_number--text'>
+                        <h3>#</h3>
                     </div>
-                   <div className='chart_release'>
-                        <h3>Release Date</h3>
-                        <h3>Streams</h3>
-                   </div>
-                    
+                    <div className='chart_week'>
+                        <h3>Week</h3>
+                    </div>
                 </div>
-            </div>
+                <div className='chart_song--container'>
+                    <div className='chart_img'>
+                         
+                    </div>
+                    <div className='chart_song'>
+                        <div className='chart_song--info'>
+                        <h3>songs</h3>
+                        </div>
+                        <div className='chart_release'>
+                           <h3>release Date</h3>
+                           <h3 className='streams'>Streams</h3>
+                        </div>  
+                    </div>
+                </div>
+            </li>
         {ChartData.map(item => (
-          <ul key={item.id}>
-            <li className='chart_container'>
-                <div className='chart_left' >
-                    <div className='chart_number'><h1>{item.id}</h1></div>
-                    <div className='chart_week'><p>{item.onChart}</p></div>
+            <li className='chart_container' key={item.id}>
+                <div className='chart_left'>
+                    <div className='chart_number'>
+                        <h1>{item.id}</h1>
+                    </div>
+                    <div className='chart_week'>
+                        <h3>{item.onChart}</h3>
+                    </div>
                 </div>
                 <div className='chart_song--container'>
                     <div className='chart_img'>
@@ -38,13 +50,12 @@ export const WeekOne = () => {
                         </div>
                         <div className='chart_release'>
                            <h3>{item.releaseDate}</h3>
-                           <h3>{item.totalStreams}</h3>
+                           <h3 className='streams'>{item.totalStreams}</h3>
                         </div>  
                     </div>
                 </div>
             </li>
-          </ul>
         ))}
-      </div>
+      </ul>
     )
 }
