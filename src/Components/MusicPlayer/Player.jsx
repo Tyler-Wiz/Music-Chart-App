a9d03b010d8cc230e2f0b1464286b4794ce8edf0
tyler <tylertooxclusive@gmail.com>
2021-11-06 18:43:52 +0000

    audiplayer css updated

--- src/Components/MusicPlayer/Player.jsx
+++ src/Components/MusicPlayer/Player.jsx
@@ -1,6 +1,7 @@
 import { Controls } from './Controls';
 import './Player.css'
-import { PlayLogic } from './PlayLogic';
+import { PlayLogic } from './PlayLogic'
+
 
 export const Player = () => {
 const [
@@ -12,42 +13,51 @@ const [
     ] = PlayLogic()
 
   return (
-    <div className='player_container'>
-        <div className="audio-player">
-            <div className='track'>
+    <div className="audio-player">
+        <div className='advert_area'>
+            <h2>Advert Postion</h2>
+        </div>
+        <div className='track_artwork'>
+            <div>
                 <img
                 className="artwork"
                 src={image}
                 alt={`track artwork for ${title} by ${artist}`}
                 />
             </div>
-            <div>
-                <h2 className="title">{title}</h2>
-                <h3 className="artist">{artist}</h3>
+            <div className='track_details'>
+                <h3 className="title">{title}</h3>
+                <p className="artist">{artist}</p>
             </div>
-            <div className="track-info">
-                <div>
-                    <Controls
-                        isPlaying={isPlaying}
-                        onPrevClick={toPrevTrack}
-                        onNextClick={toNextTrack}
-                        onPlayPauseClick={setIsPlaying}
-                    />
-                        <input
-                        type="range"
-                        value={trackProgress}
-                        step="1"
-                        min="0"
-                        max={duration ? duration : `${duration}`}
-                        className="progressBar"
-                        onChange={(e) => onScrub(e.target.value)}
-                        onMouseUp={onScrubEnd}
-                        onKeyUp={onScrubEnd}
-                        style={{ background: trackStyling }}
-                    />
-                </div>
-            </div>
+            
         </div>
+        <div className='track_progress'>
+            <input
+                type="range"
+                value={trackProgress}
+                step="1"
+                min="0"
+                max={duration ? duration : `${duration}`}
+                className="progressBar"
+                onChange={(e) => onScrub(e.target.value)}
+                onMouseUp={onScrubEnd}
+                onKeyUp={onScrubEnd}
+                style={{ background: trackStyling }}
+            />
+            <div className="artist_mobile">
+                <span className="mobile_title">{title}</span>
+                <span className="mobile_artist">{artist}</span>
+             </div>
+        </div>
+        <div className='track_controls'>
+            <Controls
+                isPlaying={isPlaying}
+                onPrevClick={toPrevTrack}
+                onNextClick={toNextTrack}
+                onPlayPauseClick={setIsPlaying}
+            />    
+        </div>
+        
     </div>
   )
 }
